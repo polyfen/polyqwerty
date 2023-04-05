278bb43833d7a97c86d5f28e2e0181bdd41803c6
xxiicode <leandrobilokapic@gmail.com>
2023-04-05 16:04:42 +0200

    head

--- index.php
+++ index.php
@@ -1,4 +1,42 @@
-<h1>Hello World, and Polyfen</h1>
-<p> for second time...</p>
-<p>main</p>
-<!-- un test para probar el stg.polynucleus.com
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- META General -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <meta property="og:type" content="website"/>
+  <meta name="author"  content="The Poly Group"/>
+
+  <!-- FAVICON -->
+
+   <!-- CSS General -->
+   <link rel="stylesheet" href="https://polynucleus.com/styles.css">
+   <link rel="stylesheet" href="app/css/styles.css">
+
+   <!-- FONTS -->
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://polynucleus.com/styles.css" type="text/css">
+    
+    <!-- PAGE-SPECIFIC META start -->
+    <meta property="og:title" content="Poly Qwerty"/>
+    <meta property="og:url" content="https://polyqwerty.com"/>
+    <meta name="keywords" content="" />
+    <meta name="description" property="og:description" content="" />
+    <meta property="og:image" content=""/>
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="Poly Qwerty" />
+    <meta name="twitter:creator" content="@ThePolyGroupCo">
+    <meta name="twitter:title" content="Your keyboard's assistant" />
+    <meta name="twitter:description" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="robots" content="index, follow">
+    <!-- PAGE-SPECIFIC META end -->
+</head>
+<body>
+    <h1>hello</h1>
+</body>
+</html>
